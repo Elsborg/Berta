c3d59bf56483229d3edcbdc00acfc3146560b4fd
Elsborg <martin.elsborg@gmail.com>
2022-05-22 12:26:39 +0200

    Kommentar tilføjet til koden

--- Javascript/navbar.js
+++ Javascript/navbar.js
@@ -1,6 +1,9 @@
+//Henter elementet med id'et og gemmer det i en variable.
 const toggleButton =  document.getElementById('toggle-button');
 const naviList = document.getElementById('navi-list');
 
+//Putter en event listener på toggleButton, så ved et click skal den
+//tilføje klassen "active", så mobil menuen bliver synlig.
 toggleButton.addEventListener('click', () => {
 naviList.classList.toggle('active');
 })
